6137ef138bc7b6cf8c800c6f43dc3a6b1cd31513
Notro <77281214+NotroDev@users.noreply.github.com>
2026-05-17 09:07:46 +0200

    Fix ghost caption buttons on Windows

--- src/FluentAvalonia/UI/Windowing/Win32/FAAppWindow.win32.cs
+++ src/FluentAvalonia/UI/Windowing/Win32/FAAppWindow.win32.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
-using Avalonia.Controls;
 using FluentAvalonia.Interop;
+using FluentAvalonia.Interop.Win32;
+using static FluentAvalonia.Interop.Win32Interop;
 
 namespace FluentAvalonia.UI.Windowing;
 
@@ -13,11 +14,25 @@ public partial class FAAppWindow
         IsWindows11 = OSVersionHelper.IsWindows11();
 
         _win32Manager = new Win32WindowManager(this);
-
+        
         // Force AppWindow into darkmode at the system level
-        Win32Interop.ApplyTheme(_win32Manager.Hwnd, true);
+        ApplyTheme(_win32Manager.Hwnd, true);
+        
+        if (IsWindows11)
+        {
+            SuppressWin11CaptionRendering(_win32Manager.Hwnd);
+        }
+        
         PseudoClasses.Add(":windows");
         PlatformFeatures = new Win32AppWindowFeatures(this);
+    }
+    
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static unsafe void SuppressWin11CaptionRendering(HWND hwnd)
+    {
+        var colorNone = 0xFFFFFFFE;
+        DwmSetWindowAttribute(hwnd, DWMWINDOWATTRIBUTE.DWMWA_CAPTION_COLOR, &colorNone, sizeof(uint));
+        DwmSetWindowAttribute(hwnd, DWMWINDOWATTRIBUTE.DWMWA_TEXT_COLOR, &colorNone, sizeof(uint));
     }
 
     private Win32WindowManager _win32Manager;
